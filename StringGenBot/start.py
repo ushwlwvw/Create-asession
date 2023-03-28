63a1e46ec8982199f1de3381955002477ca6a628
ushwlwvw <126856143+ushwlwvw@users.noreply.github.com>
2023-03-28 05:09:35 +0300

    Update start.py

--- StringGenBot/start.py
+++ StringGenBot/start.py
@@ -1,67 +1,57 @@
 from pyrogram import Client, filters
+
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
 from config import OWNER_ID
 
-
 def filter(cmd: str):
+
     return filters.private & filters.incoming & filters.command(cmd)
 
 @Client.on_message(filter("start"))
+
 async def start(bot: Client, msg: Message):
+
     me2 = (await bot.get_me()).mention
+
     await bot.send_message(
+
         chat_id=msg.chat.id,
-        text=f"""Hᴇʏ {msg.from_user.mention},
 
-Tʜɪs ɪs {me2},
-Aɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.
-
-Mᴀᴅᴇ ᴡɪᴛʜ 🖤 ʙʏ : [𝝙𝗡𝗢𝗡𝗬𝗠𝗢𝗨𝗦](tg://user?id={OWNER_ID}) !""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(text="🙄 ɢᴇɴᴇʀᴀᴛᴇ sᴇssɪᴏɴ 🙄", callback_data="generate")
-                ],
-                [
-                    InlineKeyboardButton("❣️ sᴏᴜʀᴄᴇ ❣️", url="https://github.com/AnonymousX1025/StringGenBot"),
-                    InlineKeyboardButton("🥀 ᴅᴇᴠᴇʟᴏᴩᴇʀ 🥀", user_id=OWNER_ID)
-                ]
-            ]
-        ),
-        disable_web_page_preview=True,
-    )
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
-
-from config import OWNER_ID
-
-
-def filter(cmd: str):
-    return filters.private & filters.incoming & filters.command(cmd)
-
-@Client.on_message(filter("start"))
-async def start(bot: Client, msg: Message):
-    me2 = (await bot.get_me()).mention
-    await bot.send_message(
-        chat_id=msg.chat.id,
         text=f"""**مرحبا** {msg.from_user.mention},
 
 انا بوت {me2},
+
 استطيع استخراج جلسات على 4 مكاتب
+
  مكتوب بلغة بايثون
 
 • مطور البوت • 🖤 ʙʏ : [𝝙𝗡𝗢𝗡𝗬𝗠𝗢𝗨𝗦](tg://user?id={OWNER_ID}) !""",
+
         reply_markup=InlineKeyboardMarkup(
+
             [
+
                 [
+
                     InlineKeyboardButton(text="✠ استخراج جلسات  ✠", callback_data="generate")
+
                 ],
+
                 [
+
                     InlineKeyboardButton("✠ السورس ✠️", url="https://t.me/aaaalqp"),
+
                     InlineKeyboardButton("「 𝗗𝗲𝘃𝗲𝗟𝗼𝗣𝗲𝗥 」", user_id=OWNER_ID)
+
                 ]
+
             ]
+
         ),
+
         disable_web_page_preview=True,
+
     )
+
+  
